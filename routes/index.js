d448235a4fa43eb5308aff9fdc1e2dbeae4f57a8
Isaac Sanders <isaac@isaacbfsanders.com>
2014-11-23 03:27:00 -0600

    Try to add displayName

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Surveymo' });
+  res.render('index', { title: 'Surveymo', displayName: (req.user || {}).displayName });
 });
 router.get('/bdeeed28213c1646b84ef99fe7c6f94e.txt', function(req, res) {
   res.sendFile('D:\\home\\site\\wwwroot\\routes\\bdeeed28213c1646b84ef99fe7c6f94e.txt');
