b582cadf7e057e57de73d26e0b22c9ae77aeb806
shomerrt <shomerrt@rose-hulman.edu>
2014-11-23 05:56:06 -0600

    shit broke

--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,4 @@ router.get('/', function(req, res) {
 router.get('/bdeeed28213c1646b84ef99fe7c6f94e.txt', function(req, res) {
   res.sendFile('D:\\home\\site\\wwwroot\\routes\\bdeeed28213c1646b84ef99fe7c6f94e.txt');
 });
-router.get('/herpderp', function(req, res) {
-	requestify.post('https://api.venmo.com/v1/payments', {
-			access_token: (req.user || {}).accessToken,
-			user_id: 'isaacsanders',
-			note: 'Hey bae',
-			amount: 0.01
-		})
-	.then(function(response) {
-		// Get the response body
-		response.getBody();
-	});
-});
-
-
 module.exports = router;
