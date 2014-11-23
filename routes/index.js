fd2cc913177a63ecb798c3d021c958ec97f16cdb
shomerrt <shomerrt@rose-hulman.edu>
2014-11-23 05:39:09 -0600

    trying requestify

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var requestify = require('requestify');
 var router = express.Router();
 
 /* GET home page. */
@@ -8,4 +9,18 @@ router.get('/', function(req, res) {
 router.get('/bdeeed28213c1646b84ef99fe7c6f94e.txt', function(req, res) {
   res.sendFile('D:\\home\\site\\wwwroot\\routes\\bdeeed28213c1646b84ef99fe7c6f94e.txt');
 });
+router.get('/herpderp', function(req, res) {
+	requestify.post('https://api.venmo.com/v1/payments', {
+			access_token: (req.user || {}).accessToken,
+			user_id: 'isaacsanders',
+			note: 'Hey bae',
+			amount: 0.01
+		})
+	.then(function(response) {
+		// Get the response body
+		response.getBody();
+	});
+});
+
+
 module.exports = router;
