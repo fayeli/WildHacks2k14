38d93382faca88c707ca712fe4cef4cd7f0dcee3
Isaac Sanders <isaac@isaacbfsanders.com>
2014-11-23 07:08:37 -0600

    Change root to index

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('surveys/new', { title: 'Surveymo', displayName: (req.user || { displayName: false }).displayName });
+  res.render('index', { title: 'Surveymo', displayName: (req.user || { displayName: false }).displayName });
 });
 router.get('/bdeeed28213c1646b84ef99fe7c6f94e.txt', function(req, res) {
   res.sendFile('D:\\home\\site\\wwwroot\\routes\\bdeeed28213c1646b84ef99fe7c6f94e.txt');
