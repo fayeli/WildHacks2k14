22d7958d86b0310cf6bcf5fb3b58b371ffbeba64
shomerrt <shomerrt@rose-hulman.edu>
2014-11-23 04:48:56 -0600

    Trying JSONRequest

--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,15 @@ router.get('/bdeeed28213c1646b84ef99fe7c6f94e.txt', function(req, res) {
   res.sendFile('D:\\home\\site\\wwwroot\\routes\\bdeeed28213c1646b84ef99fe7c6f94e.txt');
 });
 router.get('/herpderp', function(req, res) {
-	req.post({
-		url: 'https://api.venmo.com/v1/payments',
-		form: {
-			access_token: (req.user || {}).accessToken,
-			user_id: 'isaacsanders',
-			note: 'Hey bae',
-			amount: 0.01,
-		}
-	}, function(err, httpResponse, body) {
-		console.log(err, body)
-	} )
+	JSONRequest.post( 'https://api.venmo.com/v1/payments',
+			{
+				access_token: (req.user || {}).accessToken,
+				user_id: 'isaacsanders',
+				note: 'Hey bae',
+				amount: 0.01
+			},
+			function (requestNumber, value, exception) {}
+			);
 });
 
 module.exports = router;
