b5f1913e479e1175d1aa47cd4b5b5e6a3602c1bb
Isaac Sanders <isaac@isaacbfsanders.com>
2014-11-22 18:17:55 -0600

    Implement more mongo stuff

--- models/user.js
+++ models/user.js
@@ -1,25 +1,31 @@
 var mongoose = require('mongoose');
 var primitives = require('./models/primitives')
 
-var Kitten = mongoose.model('User', mongoose.Schema({
+var User = mongoose.model('User', mongoose.Schema({
+  displayName: String,
   username: String,
+  email: String,
+  accessToken: String,
+  refreshToken: String,
   venmoCreds: {},
   ageRange: primitives.AgeRange,
   gender: primitives.Gender,
   location: primitives.Location
-}))
+}));
 
-{
-    surveySchema: {
-        id: Integer,
-        owner: String,
-        question: String,
-        choices: [String]
-    },
-    responseSchema: {
-        id: Integer,
-        surveyId: Integer,
-        responder: String,
-        responseIndex: Integer
-    }
-}
+exports = User;
+
+// {
+//   surveySchema: {
+//     id: Integer,
+//     owner: String,
+//     question: String,
+//     choices: [String]
+//   },
+//   responseSchema: {
+//     id: Integer,
+//     surveyId: Integer,
+//     responder: String,
+//     responseIndex: Integer
+//   }
+// }
