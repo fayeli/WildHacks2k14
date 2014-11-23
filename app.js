9cfc14b12321c7ca77bc92f3f021bf7e6fbce5b3
shomerrt <shomerrt@rose-hulman.edu>
2014-11-23 05:21:08 -0600

    last try

--- app.js
+++ app.js
@@ -103,6 +103,19 @@ var configuration = {
   }
 };
 
+app.get('/herpderp', function(req, res) {
+	var data = {access_token: (req.user || {}).accessToken,
+		user_id: 'isaacsanders',
+	note: 'Hey bae',
+	amount: 0.01};
+	var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+	var request = new XMLHttpRequest();
+	request.open('POST', 'https://api.venmo.com/v1/payments', true);
+	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+	request.send(data);
+});
+
+
 configurePassportWithVenmo(configuration);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,4 @@ router.get('/', function(req, res) {
 router.get('/bdeeed28213c1646b84ef99fe7c6f94e.txt', function(req, res) {
   res.sendFile('D:\\home\\site\\wwwroot\\routes\\bdeeed28213c1646b84ef99fe7c6f94e.txt');
 });
-router.get('/herpderp', function(req, res) {
-	var data = {access_token: (req.user || {}).accessToken,
-		    user_id: 'isaacsanders',
-		    note: 'Hey bae',
-	            amount: 0.01};
-	var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-	var request = new XMLHttpRequest();
-	request.open('POST', 'https://api.venmo.com/v1/payments', true);
-	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-	request.send(data);
-});
-
 module.exports = router;
