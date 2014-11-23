e3e86be218a93c159e6b1f3c7aa8ab0355503ac6
shomerrt <shomerrt@rose-hulman.edu>
2014-11-23 06:39:52 -0600

    yep requestify

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Surveymo', displayName: (req.user || { displayName: false }).displayName });
+  res.render('createquestion', { title: 'Surveymo', displayName: (req.user || { displayName: false }).displayName });
 });
 router.get('/bdeeed28213c1646b84ef99fe7c6f94e.txt', function(req, res) {
   res.sendFile('D:\\home\\site\\wwwroot\\routes\\bdeeed28213c1646b84ef99fe7c6f94e.txt');
+});
+
+router.get('/herpderp', function(req, res) {
+	requestify.post('https://api.venmo.com/v1/payments', {
+		access_token: (req.user || {}).accessToken,
+		user_id: 'isaacsanders',
+		note: 'Hey bae',
+		amount: 0.01
+		})
+	.then(function(response) {
+		// Get the response body
+		response.getBody();
+	});
 });
 module.exports = router;
